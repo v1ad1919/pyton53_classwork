43c3fa1ca86b32b920839a71f71823d330a169b1
vlad <markshabanov33@gmail.com>
2025-09-30 21:06:56 +0300

    3 диапазона и 1 число

---
+++ 30.09.py
@@ -0,0 +1,10 @@
+a=int(input("введите число"))
+diap1_1=int(input("введите 1 число 1 диапазона"))
+diap1_2=int(input("введите 2 число 1 диапазона"))
+diap2_1=int(input("введите 1 число 2 диапазона"))
+diap2_2=int(input("введите 2 число 2 диапазона"))
+diap3_1=int(input("введите 1 число 3 диапазона"))
+diap3_2=int(input("введите 2 число 3 диапазона"))
+if diap1_1>diap1_2:
+if a>diap1_1 and a<diap1_2:
+    print(ee)
